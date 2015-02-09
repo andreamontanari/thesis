ffb508a8fadb8d915949475e997d4640726526bd
Andrea Montanari <andrea.montanari92@gmail.com>
2015-02-09 14:17:06 +0100

    Thesis
    First activity created

--- MyThesis/app/src/main/java/com/andreamontanari/mythesis/MainActivity.java
+++ MyThesis/app/src/main/java/com/andreamontanari/mythesis/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +37,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void begin(View view) {
+        Toast.makeText(this, "Pressed", Toast.LENGTH_SHORT).show();
     }
 }
